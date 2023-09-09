c4f86235d37f9a94ccc164ef3c1f6d83a8c5b001
yellowhatpro <yellowhatpro3119@gmail.com>
2023-09-09 21:50:29 +0530

    flower

--- flower-dapp/src/app/page.tsx
+++ flower-dapp/src/app/page.tsx
@@ -1,49 +1,49 @@
 "use client";
 import * as fcl from "@onflow/fcl";
-import {Navbar} from "./components";
+import { Navbar } from "./components";
 import { useEffect, useState } from "react"
-import {Poppins} from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import Login from "./login"
-import {undefined} from "zod";
 
 const poppins = Poppins(
-    {weight: ["100", "200", "300"],
-        subsets: ['latin'],
-        variable: '--font-poppins',
-    })
+  {
+    weight: ["100", "200", "300"],
+    subsets: ['latin'],
+    variable: '--font-poppins',
+  })
 
 fcl.config({
-  "accessNode.api" : "http://localhost:8888",
-  "discovery.wallet" : "http://localhost:8701/fcl/authn", // emulator endpoint
-  "app.detail.title" : "Flower",
-  "0xFlower" : "0xf8d6e0586b0a20c7"
+  "accessNode.api": "http://localhost:8888",
+  "discovery.wallet": "http://localhost:8701/fcl/authn", // emulator endpoint
+  "app.detail.title": "Flower",
+  "0xFlower": "0xf8d6e0586b0a20c7"
 })
 
 export default function Home() {
 
-    const [user, setUser] = useState({addr: ''});
+  const [user, setUser] = useState({ addr: '' });
 
-    const [posts, setPosts] = useState("");
+  const [posts, setPosts] = useState("");
 
-    useEffect(() => {
-        fcl.currentUser.subscribe(setUser);
+  useEffect(() => {
+    fcl.currentUser.subscribe(setUser);
 
-    }, []);
+  }, []);
 
-    const logIn = async (): Promise<void> => {
-        await fcl.authenticate();
-        await checkCollectionIsCreated();
-    };
-    const logOut = () => {
-        fcl.unauthenticate();
-    };
+  const logIn = async (): Promise<void> => {
+    await fcl.authenticate();
+    await checkCollectionIsCreated();
+  };
+  const logOut = () => {
+    fcl.unauthenticate();
+  };
 
-    //scripts
+  //scripts
 
-    async function checkCollectionIsCreated() {
-        const res = await fcl.query(
-            {
-                cadence: `import Flower from "../contracts/flower/Flower.cdc"
+  async function checkCollectionIsCreated() {
+    const res = await fcl.query(
+      {
+        cadence: `import Flower from "../contracts/flower/Flower.cdc"
 
 pub fun main(account: Address) : Bool {
   if  getAccount(account).getCapability(/public/collection).borrow<&Flower.Collection{Flower.ICollection}>() == nil{
@@ -54,21 +54,21 @@ pub fun main(account: Address) : Bool {
 } 
 
 `,
-                args: (arg, t) => [
-                    arg("0xf8d6e0586b0a20c7", t.Address),
-                ],
-            }
-        )
-        if (!res) {
-            await createCollection();
-        }
+        args: (arg, t) => [
+          arg("0xf8d6e0586b0a20c7", t.Address),
+        ],
+      }
+    )
+    if (!res) {
+      await createCollection();
     }
+  }
 
 
-    //transactions
-    async function createCollection() {
-        const txId = await fcl.mutate({
-            cadence: `import Flower from 0xFlower
+  //transactions
+  async function createCollection() {
+    const txId = await fcl.mutate({
+      cadence: `import Flower from 0xFlower
 
       transaction {
         prepare(account: AuthAccount){
@@ -79,33 +79,33 @@ pub fun main(account: Address) : Bool {
         log("Collection created")
       }
     }`,
-        })
-        console.log(txId)
-    }
+    })
+    console.log(txId)
+  }
 
-    return (
-        <main className={poppins.className}>
-            {
-                (user.addr) ?
+  return (
+    <main className={poppins.className}>
+      {
+        (user.addr) ?
 
-                    <div className=" min-h-screen flex-col items-center bg-catppuccin_blue5">
-                        <Navbar
-                            logout={logOut}
-                            userAcc={user.addr}/>
-                        <div className="flex flex-col items-center justify-between p-24">
+          <div className=" min-h-screen flex-col items-center bg-catppuccin_blue5">
+            <Navbar
+              logout={logOut}
+              userAcc={user.addr} />
+            <div className="flex flex-col items-center justify-between p-24">
 
-                            <div className="">
+              <div className="">
 
-                                {"post ids:" + posts}
-                            </div>
-                        </div>
-                    </div>
-                    :
-                    <div>
-                        <Login login={logIn}></Login>
-                    </div>
+                {"post ids:" + posts}
+              </div>
+            </div>
+          </div>
+          :
+          <div>
+            <Login login={logIn}></Login>
+          </div>
 
 
-            }
-        </main>)
+      }
+    </main>)
 }
